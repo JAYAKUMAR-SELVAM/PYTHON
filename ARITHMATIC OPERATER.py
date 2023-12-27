3d1159adddcb0c7e7ce898847261aa9be9aa3747
JAYAKUMAR-SELVAM <142247421+JAYAKUMAR-SELVAM@users.noreply.github.com>
2023-12-27 15:46:59 +0530

    Add files via upload

---
+++ ARITHMATIC OPERATER.py
@@ -0,0 +1,20 @@
+# ARITHMATIC OPERATER
+
+"""
++   ADDITION
+-   SUBRACTION
+*   MULTIPLICATION
+\   DIVISIOIN
+**  EXPONENTIAL(2 POWER OF 3)
+%   MODULS(FOR PRINT . AFTER VALUES)
+//  FLOOR DIVISION(FOR PRINT . BEFORE VALUES)
+"""
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+print(a//b)
