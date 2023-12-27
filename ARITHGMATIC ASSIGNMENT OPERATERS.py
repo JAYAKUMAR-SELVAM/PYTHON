2bd87315c054777b13f5003bb35ecb8a53aa6864
JAYAKUMAR-SELVAM <142247421+JAYAKUMAR-SELVAM@users.noreply.github.com>
2023-12-27 15:27:58 +0530

    Add files via upload

---
+++ ARITHGMATIC ASSIGNMENT OPERATERS.py
@@ -0,0 +1,33 @@
+# ARITHGMATIC ASSIGNMENT OPERATERS
+
+"""
++=   ADDITION
+-=   SUBRACTION
+*=   MULTIPLICATION
+\=   DIVISIOIN
+**=  EXPONENTIAL(2 POWER OF 3)
+%=  MODULS(FOR PRINT . AFTER VALUES)
+//=  FLOOR DIVISION(FOR PRINT . BEFORE VALUES)
+
+"""
+a=130
+a+=5
+print(a)
+
+a-=5
+print(a)
+
+a*=5
+print(a)
+
+a/=5
+print(a)
+
+a**=5
+print(a)
+
+a%=5
+print(a)
+
+a//=5
+print(a)
