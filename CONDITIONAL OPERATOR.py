d2d47437e6e5806c6b61b373c6a167b1dde28070
JAYAKUMAR-SELVAM <142247421+JAYAKUMAR-SELVAM@users.noreply.github.com>
2023-12-29 21:13:02 +0530

    Add files via upload

---
+++ CONDITIONAL OPERATOR.py
@@ -0,0 +1,32 @@
+# conditional operator or relational opereater
+
+"""
+== equal to equal to
+!= not equql to
+< greater the 
+> lesser than 
+<= greater than or equal to
+>= lesser than or equal to
+
+"""
+
+a=10
+b=13
+
+# == equal to equal to
+print(a==b)
+
+# != not equql to
+print(a!=b)
+
+# < greater the 
+print(a<b)
+
+# > lesser than 
+print(a>b)
+
+# <= greater than or equal to
+print(a<=b)
+
+# >= lesser than or equal to
+print(a>=b)
