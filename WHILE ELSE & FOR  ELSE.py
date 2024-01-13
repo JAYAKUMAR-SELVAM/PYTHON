683f6c53732178e5c003422fffced1ed1cc0dec5
JAYAKUMAR-SELVAM <142247421+JAYAKUMAR-SELVAM@users.noreply.github.com>
2024-01-13 11:47:14 +0530

    Add files via upload

---
+++ WHILE ELSE & FOR  ELSE.py
@@ -0,0 +1,25 @@
+# WHILE ELSE & FOR ELSE
+
+"""
+here the if condition is satisfied the ELSE condition will not work
+"""
+
+i=1
+while i<=10:
+    #if i==8:
+        #break
+    print(i)
+    i=i+1
+else:
+    print("loop is completed")
+
+print("\n-----------------------------------------------------------\n")
+
+i=1
+for i in range(1,10):
+    #if i==8:
+        #break
+    print(i)
+    i=i+1
+else:
+    print("loop is completed")
